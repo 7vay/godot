4013d96f2ab8fe18b2edbbe61beee4030235bdfe
yo112273 <e.thiessen@tu-braunschweig.de>
2025-01-12 09:28:50 +0100

    hills

--- schule/scripts/gen_world.gd
+++ schule/scripts/gen_world.gd
@@ -10,14 +10,18 @@ var height : int = 200
 var sourceIdWater = 0 #wasser id
 var waterAtlas = Vector2i(0,0) #position des Wassers auf dem Spritesheet
 var sourceIdGras = 1
-var grasAtlas = Vector2i(1,1) #erstmal random gras tile genomme   SPÄTER ÄNDEERUNG ZU TERRAIN
+var hillsTreppeAtlas = Vector2i(9,4)
+var sourceIdHills = 2
 
 #LAYER
 var waterLayer = 0
 var grasLayer = 1
+var hillsLayer = 2
 
 var grasTilesArr = []
 var terrainGrasInt = 0
+var hillsTilesArr = []
+var terrainHillsInt = 1
 
 func _ready():
 	noise = noiseHeight.noise #gibt das Generierte Noise in eine Variable über
@@ -26,11 +30,19 @@ func _ready():
 
 func generate_world():
 	for x in range(-width/2, width/2):
-		for y in range(-height/2, width/2):
+		for y in range(-height/2, height/2):
 			var noise_val : float = noise.get_noise_2d(x,y)
-			if noise_val >= 0.0: #Land
+			if noise_val >= -0.25: #Land
 				grasTilesArr.append(Vector2i(x,y))
+				pass
+			if noise_val == 0.25:
+				hillsTilesArr
+				pass
+			elif noise_val > 0.25:
+				hillsTilesArr.append(Vector2i(x,y))
 				pass
 				
 			tileMap.set_cell(waterLayer ,Vector2(x,y), sourceIdWater, waterAtlas) #Wasser soll ja überhall hin also lassen packen wir dasl "drunter"
 	tileMap.set_cells_terrain_connect(grasLayer, grasTilesArr, terrainGrasInt, 0)
+	tileMap.set_cells_terrain_connect(hillsLayer, hillsTilesArr, terrainHillsInt, 0)
+	
