5831baa2b52ab02f6702544f8ab228e8300316b1
7vay <bennet.walter@gymszbad.de>
2025-02-17 08:25:49 +0100

    updated mainmenu

--- schule/scripts/homescreen/main_menu.gd
+++ schule/scripts/homescreen/main_menu.gd
@@ -1,27 +1,26 @@
 extends Control
 
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
 
 
 func _on_starten_button_pressed() -> void:
 	print("Starten wurde gedrückt")
+	#neue Welt wird generiert...
 
 
 func _on_laden_button_pressed() -> void:
 	print("Laden wurde gedrückt")
+	#verschiedene gespeicherte Welten laden
 
 
 func _on_optionen_button_pressed() -> void:
 	print("Optionen wurde gedrückt")
-
+	#Optionenfenster starten und einstellen
 
 func _on_beenden_button_pressed() -> void:
 	print("Beenden wurde gedrückt")
+	#Projekt schließen
+	
+#Einstellungenfenster vielleicht noch mit Escape
