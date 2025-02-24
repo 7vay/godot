9a961406825cf73f736adbc53800640e7a34122c
7vay <bennet.walter@gymszbad.de>
2025-02-24 20:03:24 +0100

    start von ladebildschirm

--- schule/scripts/mainmenu/main_menu.gd
+++ schule/scripts/mainmenu/main_menu.gd
@@ -5,7 +5,7 @@ func _on_starten_pressed() -> void:
 
 
 func _on_laden_pressed() -> void:
-	print("Hilfe Laden")
+	get_tree().change_scene_to_file("res://scenes/ladebildschirm.tscn")
 
 
 func _on_beenden_pressed() -> void:
---
+++ schule/world_resource.gd
@@ -0,0 +1,5 @@
+extends Resource
+class_name WorldResource
+
+@export var name: String = ""
+@export var playtime: String = ""
